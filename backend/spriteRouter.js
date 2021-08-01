40668ba3abf750cce669352fcac0e1b121782270
TheJeme <eemelijoonatan@gmail.com>
2021-08-01 20:56:16 +0300

    first

---
+++ backend/spriteRouter.js
@@ -0,0 +1,12 @@
+const spriteRouter = require("express").Router();
+const seedrandom = require("seedrandom");
+
+spriteRouter.get("/:id", async (request, response) => {
+  const id = request.params.id;
+  const rng = seedrandom(id);
+  const filepath = `sprites/${Math.round(rng() * 1000)}.png`;
+  console.log(filepath);
+  response.sendFile(filepath, { root: __dirname });
+});
+
+module.exports = spriteRouter;
