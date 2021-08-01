91d673b2f1785b0057a9d68b4ccb2b8df63e3d95
TheJeme <eemelijoonatan@gmail.com>
2021-08-02 00:36:12 +0300

    frontend

--- backend/spriteRouter.js
+++ backend/spriteRouter.js
@@ -1,9 +1,10 @@
 const spriteRouter = require("express").Router();
 const seedrandom = require("seedrandom");
+const hash = require('object-hash');
 
 spriteRouter.get("/:id", async (request, response) => {
   const id = request.params.id;
-  const rng = seedrandom(id);
+  const rng = seedrandom(hash(id));
   const filepath = `sprites/${Math.floor(rng() * 1000)}.png`;
   response.sendFile(filepath, { root: __dirname });
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [imageID, setImageID] = useState(Math.floor(Math.random() * 10000))
+
+  const changeImage = (value) => {
+    setImageID(value || Math.floor(Math.random() * 100000))
+  }
+
   return (
     <div className="App">
-
+      <h1>Spryte</h1>
+      <p>Spryte is easy to use app that generates unique symmetric avatars. 
+        Provide any kind of text, such as email, username, id, or whatever you like and get back a nice avatar.</p>
+      <hr/>
+      <input placeholder="Type here" onChange={(e) =>changeImage(e.target.value)}/>
+      <h2>Avatar</h2>
+      <img alt="avatar" src={`http://localhost:3003/${imageID}`}/>
+      <p>http://localhost:3003/{imageID}</p>
     </div>
   );
 }
