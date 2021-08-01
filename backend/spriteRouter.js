2f99abd6d454fb3da2aefd2f070fea3e03ca449c
TheJeme <eemelijoonatan@gmail.com>
2021-08-01 21:16:33 +0300

    renamed images

--- backend/spriteRouter.js
+++ backend/spriteRouter.js
@@ -5,7 +5,6 @@ spriteRouter.get("/:id", async (request, response) => {
   const id = request.params.id;
   const rng = seedrandom(id);
   const filepath = `sprites/${Math.round(rng() * 1000)}.png`;
-  console.log(filepath);
   response.sendFile(filepath, { root: __dirname });
 });
 
