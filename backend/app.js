c82b6511d50ac95467591e1b8347293a5627ca1b
TheJeme <eemelijoonatan@gmail.com>
2021-08-05 13:28:35 +0300

    added cors

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const spriteRouter = require("./spriteRouter");
 
+app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 
